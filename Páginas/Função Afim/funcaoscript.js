bd3b40d595145974c3525dd28490bf21cd7804a9
Carlos Freitas <cf3337727@gmail.com>
2024-10-02 20:51:40 -0300

    Update funcaoscript.js

--- Páginas/Função Afim/funcaoscript.js
+++ Páginas/Função Afim/funcaoscript.js
@@ -1,4 +1,4 @@
-/* Função para Calcular Função Afim */
+// Função para Calcular Função Afim
 
 function calc(){
     var resposta = document.getElementById('resposta')
@@ -9,18 +9,42 @@ function calc(){
     var a = Number(txta.value)
     var b = Number(txtb.value)
 
-/* Função Crescente ou Decrescente / Coeficiente Linear */
-if (a > 0){
-    resposta.innerHTML += `<br>Função <strong>Crescente</strong><br> Coeficiente Linear: ${b}<br>`
-} else{
-    resposta.innerHTML += `<br>Função <strong>Decrescente</strong><br> Coeficiente Linear: ${b}<br>`
-}
+// Limpa Texto
+    resposta.innerText = ''
 
-/* Condição caso valor de B estiver negativo */
+// Função Crescente, Decrescente ou Nula caso seja uma Função Constante
+    if (a == 0){
+        resposta.innerHTML += ''
+    } else if (a > 0){
+        resposta.innerHTML += `<br>Função <strong>Crescente</strong><br>`
+    } else{
+        resposta.innerHTML += `Função <strong>Decrescente</strong><br>`
+    }
+
+// Função Constante, Identidade ou Linear
+    if (a == 0){
+        resposta.innerHTML += `Função <strong>Constante</strong><br>`
+    } else if (a == 1 && b == 0){
+        resposta.innerHTML += `Função <strong>Identidade</strong><br>`
+    } else if (b == 0){
+        resposta.innerHTML += `Função <strong>Linear</strong><br>`
+    }
+
+// Coeficiente Linear
+    if (value == "menos"){
+        resposta.innerHTML += `Coeficiente Linear: <strong>-${b}</strong><br>`
+    } else{
+        resposta.innerHTML += `Coeficiente Linear: <strong>${b}</strong><br>`
+    }
+
+// Erro, caso valor de 'B' seja Negativo.
     if (b < 0){
         resposta.innerHTML = `[ERRO]: Não coloque o valor de B negativo. Use o selecionamento de Mais ou Menos.`
         resposta.style.color = 'red'
-    } else if (value == "mais"){
+    } 
+
+// Função Afim
+    else if (value == "mais"){
         for (var contador = -3; contador <= 3; contador++){
             var y = (a * contador) + b
             resposta.style.color = 'black'
@@ -35,8 +59,7 @@ if (a > 0){
     }
 }
 
-
-/* Função para Limpar Texto Escrito */
+// Função para Limpar Texto Escrito
 
 function limpar(){
     resposta.innerHTML = ''
